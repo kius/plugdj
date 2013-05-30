1c02ce286d0b4b9583b111dd5fe33410aabc12f6
kius <bberwian@ig.com.br>
2013-05-30 17:10:36 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,17 +1,17 @@
 API.addEventListener(API.USER_JOIN, join);
 function join(user)
 {
-API.sendChat("@" + user.username + " Enjoy your stay in the FiM room!")
+API.sendChat("@" + user.username + " Seja bem vindo a sala Play4Ever !!!")
 }
  
 API.addEventListener(API.USER_LEAVE, leave);
 function leave(user)
 {
-API.sendChat(user.username + " left the room")
+API.sendChat(user.username + " saiu da sala...")
 }
  
 API.addEventListener(API.MOD_SKIP, skip);
 function skip(user)
 {
-API.sendChat("Sorry, your song has violated one of the room's rules.")
+API.sendChat("Desculpe, sua musica violou alguma regra da sala.")
 }
